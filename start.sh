777ff2d32cd83177f2ca8e4fa0da0e225d030154
Kevin Fox <kevin@efox.cc>
2016-09-09 15:42:41 -0700

    Add shutdown hook.

--- start.sh
+++ start.sh
@@ -15,4 +15,4 @@ if [ "x$START_WAIT_DONE_FILE" != "x" ]; then
 	touch "$START_WAIT_DONE_FILE"
 fi
 wait $pid
-[ -x /etc/shutdown.sh ] && /etc/shutdown.sh
+[ -f /etc/shutdown.sh ] && bash /etc/shutdown.sh
